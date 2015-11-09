57c0474a8e161dbfb9a26f1614d28824b68f0b09
Anna Tomka <tomkaanna@gmail.com>
2015-11-09 11:01:47 +0100

    Remove node_modules directory from repository

--- node_modules/angular2/angular2.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './core';
-export * from './profile';
-export * from './lifecycle_hooks';
-export * from './bootstrap';
-import "./manual_typings/globals-es6.d.ts";
--- node_modules/angular2/src/core/application.d.ts
+++
@@ -1,8 +0,0 @@
-import { Provider } from './di';
-import { Type } from 'angular2/src/core/facade/lang';
-import { Promise } from 'angular2/src/core/facade/async';
-import { ComponentRef } from './linker/dynamic_component_loader';
-export { APP_COMPONENT, APP_ID } from './application_tokens';
-export { platform } from './application_common';
-export { PlatformRef, ApplicationRef, applicationCommonProviders, createNgZone, platformCommon, platformProviders } from './application_ref';
-export declare function bootstrap(appComponentType: any, appProviders?: Array<Type | Provider | any[]>): Promise<ComponentRef>;
